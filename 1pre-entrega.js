f00219987b61bdf3d7a8f82bbc01f8c2c4a82851
Vanesa Porcelli <vanesaporcelli@gmail.com>
2023-04-03 14:30:58 -0300

    2preentrega

---
+++ 1pre-entrega.js
@@ -0,0 +1,117 @@
+let nombreUsuario = prompt('Bienvenido! Ingresa tu nombre para continuar');
+while (nombreUsuario == '') {
+    alert('Nombre invalido');
+    nombreUsuario = prompt('Ingresa tu nombre');
+}
+alert(nombreUsuario + ', gracias por elegirnos! Te invitamos a recorrer nuestra tienda.');
+
+let total = 0;
+let mensaje = prompt(nombreUsuario + ' quieres comprar un producto? (s-si / n-no)');
+while (mensaje == 's' || mensaje == 'S') {
+    let producto = prompt('1-Nueces Peladas\n2-Almendras Tostadas\n3-Avellanas\n4-Maní con Cáscara\n5-Lentejas\n6-Porotos\n7-Maíz\n8-Porotos de Soja');
+    switch (producto) {
+        case '1':
+            alert('Agregaste Nueces Peladas al carito de compras... $ 1200');
+            sumarTotal(1200);
+            break;
+        case '2':
+            alert('Agregaste Almendras Tostadas al carito de compras... $ 1500');
+            sumarTotal(1500);
+            break;
+        case '3':
+            alert('Agregaste Avellanas al carito de compras... $ 1600');
+            sumarTotal(1600);
+            break;
+        case '4':
+            alert('Agregaste Maní con Cáscara al carito de compras... $ 1100');
+            sumarTotal(1100);
+            break;
+        case '5':
+            alert('Agregaste Lentejas al carito de compras... $ 900');
+            sumarTotal(900);
+            break;
+        case '6':
+            alert('Agregaste Porotos al carito de compras... $ 950');
+            sumarTotal(950);
+            break;
+        case '7':
+            alert('Agregaste Maíz al carito de compras... $ 750');
+            sumarTotal(750);
+            break;
+        case '8':
+            alert('Agregaste Porotos de Soja al carito de compras... $ 980');
+            sumarTotal(980);
+            break;
+
+        default:
+            alert('Codigo inexistente');
+            break;
+    }    
+    mensaje = prompt('Desea comprar otro producto? (s-si / n-no)');
+} 
+
+    if(total > 0){
+        gestionarEnvio();
+        gestionarPago();
+        alert('Muchas gracias ' + nombreUsuario + ' por tu compra. En instantes nos pondremos en contacto para finalizar tu pedido.');
+    }
+
+
+
+
+
+
+
+    // FUNCIONES UTILIZADAS
+
+    function gestionarEnvio() {
+        alert(nombreUsuario + ' Agrega por favor tu método de envío:');
+        let envio = prompt('1-Envío a domicilio ... $1200\n2-retiro en el local ...Gratis!');
+        switch (envio) {
+            case '1':
+                alert('Agregaste Envío a domicilio... $ 1200');
+                sumarTotal(1200);
+                break;
+            case '2':
+                alert('Retirá por nuestro local sin costo adicional!... $ Envío Gratis!');
+                sumarTotal(0);
+                break;
+            default:
+                alert('No entendi la respueta');
+                gestionarEnvio();
+                break;
+        }
+    }
+
+    function gestionarPago() {
+        alert(nombreUsuario + ' Agrega por favor tu medio de pago:');
+        let envio = prompt('1-Efectivo -Contra entrega ($100 descuento )\n2-Tarjeta de credito ($50 recargo)\n3-Tarjeta de débito');
+        switch (envio) {
+            case '1':
+                alert('Abonas en efectivo. Tenes un descuento de $100');
+                restarTotal(100);
+                break;
+            case '2':
+                alert('Abonas con tarjeta de crédito.Tenés un recargo de $50');
+                sumarTotal(50);
+                break;
+            case '3':
+                alert('Abonas con débito!');
+                sumarTotal(0);
+                break;
+            default:
+                alert('No entendi la respueta');
+                gestionarEnvio();
+                break;
+        }
+    }
+
+    function sumarTotal(precio) {
+        total = total + precio;
+        alert('Llevas gastado $' + total);
+    }
+
+    function restarTotal(precio) {
+        total = total - precio;
+        alert('Llevas gastado $' + total);
+    }
--- ecommerce.js
+++ ecommerce.js
@@ -1,4 +1,90 @@
+//CLASES Y VARIABLES
+class Producto{
+    constructor(id,nombre,precio, categoria){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+}
+let productos =[
+    new Producto ( 1, "Nueces",1200,"Frutos Secos"),
+    new Producto ( 2, "Almendras", 1500, "Frutos Secos"),
+    new Producto ( 3, "Avellanas", 1600, "Frutos Secos"),
+    new Producto ( 4, "Mani",1100, "Frutos Secos"),
+    new Producto ( 5, "Lentejas", 900, "Legumbres"),
+    new Producto ( 6, "Porotos", 950, "Legumbres"),
+    new Producto ( 7, "Maiz", 750, "Legumbres"),
+    new Producto ( 8, "Garbanzos", 980,"Legumbres")
+]
 
+let carrito = [];
+
+// OPCION 1 COMPRAR PRODUCTOS:
+const comprarProductos = () =>{
+    let respuesta = '';
+    
+    do{
+        let nombreProducto = prompt(generarMenuProductos());
+
+        let producto = productoExiste(nombreProducto);
+
+        //Si el producto existe dentro del array se lo agrega
+        if ( producto ){
+            alert(`El producto agregado es: ${producto.nombre}`);
+            carrito.push(producto);
+        }else{
+            //Si no existe, se muestra un mensaje de error
+            alert(`El producto no existe! Escriba bien el nombre`);
+            continue;
+        }
+
+        respuesta = prompt(`¿Quiere comprar otro producto? (SI - NO)`);
+    }while(respuesta.toLowerCase() != "no");
+        //gestionarEnvio(); 
+    mostrarCarritoTotal(); // que incluya la suma del envio
+    alert("Muchas gracias por su compra. Nos estaremos contactando para realizar la entrega.")
+}
+
+
+//ARMA LA LISTA DE PRODUCTOS
+const generarMenuProductos = () =>{
+    let menu =`Estos productos tenemos para ofrecerte\nIngrese el nombre del producto:\n`
+    productos.forEach(producto =>{
+        menu+=` ${producto.id} - ${producto.nombre} - $${producto.precio} \n`
+    });
+    menu+=`\nIngrese el nombre del producto que quiere comprar.`
+    return menu;
+}
+
+//VALIDA SI EL PRODUCTO ESTA EN LA LISTA
+const productoExiste = (nombre) =>{
+    return productos.find(producto => producto.nombre.toLowerCase() === nombre.toLowerCase());
+}
+
+//MOSTRAR CARRITO: SUMA DE PRECIOS DE PRODUCTOS:
+const mostrarCarritoTotal = () =>{
+    let texto = `El resumen de su compra es: \n\n`
+    carrito.forEach(item =>{
+        texto += `* ${item.nombre} - $${item.precio} \n`
+    });
+    texto += `\n\nEl Subtotal es : $${calcularTotal()}`;
+    alert(texto);
+}
+
+//CALCULA SUBTOTAL DE PRODUCTOS
+const calcularTotal = () =>{
+    let total = 0;
+    carrito.forEach(producto =>{
+        total+=producto.precio;
+    })
+    return total;
+}
+
+
+// **PROGRAMA**
+
+alert(`¡Bienvenido a Tukura!`)
 let nombreUsuario = prompt('Bienvenido! Ingresa tu nombre para continuar');
 while (nombreUsuario == '') {
     alert('Nombre invalido');
@@ -6,115 +92,31 @@ while (nombreUsuario == '') {
 }
 alert(nombreUsuario + ', gracias por elegirnos! Te invitamos a recorrer nuestra tienda.');
 
-let total = 0;
-let mensaje = prompt(nombreUsuario + ' quieres comprar un producto? (s-si / n-no)');
-while (mensaje == 's' || mensaje == 'S') {
-    let producto = prompt('1-Nueces Peladas\n2-Almendras Tostadas\n3-Avellanas\n4-Maní con Cáscara\n5-Lentejas\n6-Porotos\n7-Maíz\n8-Porotos de Soja');
-    switch (producto) {
-        case '1':
-            alert('Agregaste Nueces Peladas al carito de compras... $ 1200');
-            sumarTotal(1200);
-            break;
-        case '2':
-            alert('Agregaste Almendras Tostadas al carito de compras... $ 1500');
-            sumarTotal(1500);
-            break;
-        case '3':
-            alert('Agregaste Avellanas al carito de compras... $ 1600');
-            sumarTotal(1600);
-            break;
-        case '4':
-            alert('Agregaste Maní con Cáscara al carito de compras... $ 1100');
-            sumarTotal(1100);
-            break;
-        case '5':
-            alert('Agregaste Lentejas al carito de compras... $ 900');
-            sumarTotal(900);
-            break;
-        case '6':
-            alert('Agregaste Porotos al carito de compras... $ 950');
-            sumarTotal(950);
-            break;
-        case '7':
-            alert('Agregaste Maíz al carito de compras... $ 750');
-            sumarTotal(750);
-            break;
-        case '8':
-            alert('Agregaste Porotos de Soja al carito de compras... $ 980');
-            sumarTotal(980);
-            break;
+let respuesta = parseInt(prompt(`Que operacion va a realizar, escriba el numero:
+1 - Comprar productos.
+2 - Registrar productos.
+3 - Salir`));
 
+while(respuesta != 3){
+
+    switch(respuesta){
+        case 1:
+            alert(`Ingresando al menu de compra...`);
+            comprarProductos();
+            break;
+        case 2:
+            alert(`Ingresando al registro de productos...`);
+            //proximamente
+            break;
         default:
-            alert('Codigo inexistente');
-            break;
-    }    
-    mensaje = prompt('Desea comprar otro producto? (s-si / n-no)');
-} 
-
-    if(total > 0){
-        gestionarEnvio();
-        gestionarPago();
-        alert('Muchas gracias ' + nombreUsuario + ' por tu compra. En instantes nos pondremos en contacto para finalizar tu pedido.');
+            alert(`Opcion incorrecta!`);
+            break
     }
+    respuesta = parseInt(prompt(`Que operacion va a realizar, escriba el numero
+1 - Comprar productos.
+2 - Registrar productos.
+3 - Salir`));
+}
 
-
-
-
-
-
-
-    // FUNCIONES UTILIZADAS
-
-    function gestionarEnvio() {
-        alert(nombreUsuario + ' Agrega por favor tu método de envío:');
-        let envio = prompt('1-Envío a domicilio ... $1200\n2-retiro en el local ...Gratis!');
-        switch (envio) {
-            case '1':
-                alert('Agregaste Envío a domicilio... $ 1200');
-                sumarTotal(1200);
-                break;
-            case '2':
-                alert('Retirá por nuestro local sin costo adicional!... $ Envío Gratis!');
-                sumarTotal(0);
-                break;
-            default:
-                alert('No entendi la respueta');
-                gestionarEnvio();
-                break;
-        }
-    }
-
-    function gestionarPago() {
-        alert(nombreUsuario + ' Agrega por favor tu medio de pago:');
-        let envio = prompt('1-Efectivo -Contra entrega ($100 descuento )\n2-Tarjeta de credito ($50 recargo)\n3-Tarjeta de débito');
-        switch (envio) {
-            case '1':
-                alert('Abonas en efectivo. Tenes un descuento de $100');
-                restarTotal(100);
-                break;
-            case '2':
-                alert('Abonas con tarjeta de crédito.Tenés un recargo de $50');
-                sumarTotal(50);
-                break;
-            case '3':
-                alert('Abonas con débito!');
-                sumarTotal(0);
-                break;
-            default:
-                alert('No entendi la respueta');
-                gestionarEnvio();
-                break;
-        }
-    }
-
-    function sumarTotal(precio) {
-        total = total + precio;
-        alert('Llevas gastado $' + total);
-    }
-
-    function restarTotal(precio) {
-        total = total - precio;
-        alert('Llevas gastado $' + total);
-    }
 
 
